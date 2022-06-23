0d8688230a39d722ae986dc0caffc8c8b70f77f0
Gerguevara <33437557+Gerguevara@users.noreply.github.com>
2022-06-22 22:41:22 -0600

    created card

--- src/context/GlobalContex.js
+++ src/context/GlobalContex.js
@@ -1,5 +1,16 @@
 import {createContext} from 'react';
 //shortcut rcontext
 export const GlobalContext = createContext({
-    task: []
+    tasks: [ {
+        id: "1",
+        title: "some title",
+        description: "some description",
+        done: false,
+      },
+      {
+        id: "2",
+        title: "some title",
+        description: "some description",
+        done: false,
+      },]
 });
